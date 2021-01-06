0d6776f3ad8ebef94632ec8a04ec9a1d7efd4dbc
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 17:49:27 +0100

    console träning med if,else och else if

---
+++ main.js
@@ -0,0 +1,56 @@
+/* global console, alert */
+
+
+/*
+var ticketPrice = 2000;
+
+if (ticketPrice < 1500){
+
+    console.log("Yes Its Cheap");
+}
+*/
+/*
+var ticketPrice = 1000;
+
+if (ticketPrice < 1500) {
+
+    console.log("Yes Its Cheap");
+}
+*/
+
+/*
+var ticketPrice = 2000; // Ticket Price Variable
+
+if (ticketPrice < 1500){ // If Ticket Cheaper Than 1500
+
+    console.log("Yes Its Cheap"); //  Display Message That Its Cheap
+
+} else{
+
+    console.log("No Its Expensive") //  Display Message That Its Expensive
+}
+*/
+
+/*
+var ticketPrice = 800; // Ticket Price Variable
+
+if (ticketPrice < 1500) { // If Ticket Cheaper Than 1500
+
+    console.log("Yes Its Cheap"); //  Display Message That Its Cheap
+
+} else {
+
+    console.log("No Its Expensive"); //  Display Message That Its Expensive
+}
+*/
+
+var ticketPrice = 800; // Ticket Price Variable
+
+if (ticketPrice < 1500) { // If Ticket Cheaper Than 1500
+    console.log("Yes Its Cheap"); //  Display Message That Its Cheap
+
+} else if (ticketPrice == 2000) { // If The Ticker Price Is 2000
+    console.log("Yes Its Good Price"); // Display Massage That Its Good
+
+} else {
+    console.log("No Its Expensive"); //  Display Message That Its Expensive
